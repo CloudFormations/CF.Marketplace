bac6476680df1c165a92c010df499205926339dd
CFToby-Andrew <toby.andrew@cloudformations.org>
2025-07-17 14:21:15 +0100

    Update postDeploymentsScript.ps1

--- products/cf.cumulus/temp/postDeploymentsScript.ps1
+++ products/cf.cumulus/temp/postDeploymentsScript.ps1
@@ -71,10 +71,10 @@ Install-Module -Name Az.DataFactory -Force -Scope CurrentUser
 Install-Module -Name azure.datafactory.tools -Scope CurrentUser -Force
 Install-Module -Name Az.Accounts -MinimumVersion 2.2.0 -Force -Scope CurrentUser
 
-Import-Module -Name Az
-Import-Module -Name Az.DataFactory
-Import-Module -Name azure.datafactory.tools
-Import-Module -Name Az.Accounts
+Import-Module -Name Az -Verbose
+Import-Module -Name Az.DataFactory -Verbose
+Import-Module -Name azure.datafactory.tools -Verbose
+Import-Module -Name Az.Accounts -Verbose
 
 Write-Host "Installed required modules."
 
